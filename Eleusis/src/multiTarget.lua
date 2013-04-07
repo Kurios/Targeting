8737f0e15d4101a6420ece08ec9406011c828d78
Kurios <kuriosly@gmail.com>
2013-04-07 10:20:30 -0400

    Im Horribly Slow

--- Eleusis/src/multiTarget.lua
+++ Eleusis/src/multiTarget.lua
@@ -1,8 +1,24 @@
 sysTar = sysTar or {}
 
+--Our targets are stored in a simple linked list, with piority in front.
 sysTar.targets = sysTar.targets or {}
 
+function sysTar.setTargetFront(target)
+	local targetnode = {"target"=target,"next"=sysTar.targets}
+	sysTar.targets = targetnode	
+end
+
+function sysTar.setTargetBack(target)
+	local targetnode = {"target"=target,"next"=sysTar.targets}
+	local node = sysTar.targets
+	while (node.next) do
+		node = node.next
+	end
+	node.next = targetnode
+end
+
 --Returns current target
-function sysTar.target()
-	return targets[0]
+function sysTar.target(num)
+	num = num or 0
+	return targets[num]
 end
